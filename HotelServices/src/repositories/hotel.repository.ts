5a5880092075aba00e693f2af499c9fcb4692e7a
NRMT-MAIN <sahunirmit.main@gmail.com>
2025-07-14 14:23:39 +0530

    Added Base Repository

--- HotelServices/src/repositories/hotel.repository.ts
+++ HotelServices/src/repositories/hotel.repository.ts
@@ -2,68 +2,43 @@ import logger from "../config/logger.config";
 import Hotel from "../db/models/hotel.model";
 import { createHotelDTO, updateHotelDTO } from "../dto/hotel.dto";
 import { NotFoundError } from "../utils/Error/app.error";
+import BaseRepository from "./base.repository";
 
-export async function createHotel(hotelData: createHotelDTO){
-    const hotel = await Hotel.create(hotelData) ; 
 
-    logger.info("Hotel created : " , hotel.id) ; 
-    return hotel ; 
-}
-
-export async function getHotelById(id : number){
-    const hotel = await Hotel.findByPk(id) ; 
-
-    if(!hotel){
-        logger.error(`Hotel not found: ${id}`) ; 
-        throw new NotFoundError(`Hotel with id ${id} not found`) ; 
+class HotelRepository extends BaseRepository<Hotel> { 
+    constructor(){
+        super(Hotel) ; 
     }
 
-    logger.info(`Hotel found : ${hotel.id}`) ; 
-    return hotel ; 
-}
+    async getAll() {
+        const hotels = await Hotel.findAll({
+            where : {
+                deletedAt : null
+            }
+        });
 
-
-export async function getAllHotels() {
-    const hotels = await Hotel.findAll({
-        where : {
-            deletedAt : null
+        if(!hotels) {
+            logger.error(`No hotels found`);
+            throw new NotFoundError(`No hotels found`);
         }
-    });
 
-    if(!hotels) {
-        logger.error(`No hotels found`);
-        throw new NotFoundError(`No hotels found`);
+        logger.info(`Hotels found: ${hotels.length}`);
+        return hotels;
     }
 
-    logger.info(`Hotels found: ${hotels.length}`);
-    return hotels;
-}
+    async softDelete(id : number) {
+        const hotel = await Hotel.findByPk(id) ; 
 
-export async function deleteHotelById(id : number){
-    const hotels = await Hotel.destroy({
-        where : {
-            id
+        if(!hotel) {
+            logger.error(`Hotels not found ${id}`);
+            throw new NotFoundError(`Hotel with id ${id} not found`);
         }
-    })
-}
 
-export async function updateHotel(id: number , hotelData : updateHotelDTO){
-    const hotel = await Hotel.update( hotelData , {
-        where : {
-            id : id
-        }
-    })
-}
-export async function softDeleteHotel(id : number) {
-    const hotel = await Hotel.findByPk(id) ; 
-
-    if(!hotel) {
-        logger.error(`Hotels not found ${id}`);
-        throw new NotFoundError(`Hotel with id ${id} not found`);
+        hotel.deletedAt = new Date() ; 
+        await hotel.save() ; //save the changes to the database
+        logger.info(`Hotel Soft Deleted : ${hotel.id}`) ; 
+        return true ; 
     }
-
-    hotel.deletedAt = new Date() ; 
-    await hotel.save() ; //save the changes to the database
-    logger.info(`Hotel Soft Deleted : ${hotel.id}`) ; 
-    return true ; 
 }
+
+export default HotelRepository ; 
--- HotelServices/src/services/hotel.services.ts
+++ HotelServices/src/services/hotel.services.ts
@@ -1,5 +1,5 @@
 import { createHotelDTO, updateHotelDTO } from "../dto/hotel.dto";
-import { createHotel, deleteHotelById, getAllHotels, getHotelById, softDeleteHotel, updateHotel } from "../repositories/hotel.repository";
+import HotelRepository from "../repositories/hotel.repository";
 import { BadRequestError } from "../utils/Error/app.error";
 
 const blockListedAddresses = [
@@ -7,6 +7,8 @@ const blockListedAddresses = [
     "438 Elm St" , 
     "SSB JSAK"
 ]
+
+const hotelRepository = new HotelRepository() ; 
 
 export function isAddressBlockListed(address: string): boolean {
     return blockListedAddresses.includes(address) ; 
@@ -16,27 +18,27 @@ export async function createHotelService(hotelData: createHotelDTO){
     if(isAddressBlockListed(hotelData.address)){
         throw new BadRequestError("Address is blocklisted") ; 
     }
-    const hotel = await createHotel(hotelData) ; 
+    const hotel = await hotelRepository.create(hotelData) ; 
     return hotel ; 
 }
 
 export async function getHotelByIdServices(id : number){
-    const hotel = await getHotelById(id) ; 
+    const hotel = await hotelRepository.findById(id) ; 
     return hotel ; 
 }
 
 export async function getAllHotelsService() {
-    const hotels = await getAllHotels();
+    const hotels = await hotelRepository.findAll();
     return hotels;
 }
 
 export async function deleteHotelService(id : number){
     //const hotels = await deleteHotelById(id) ; 
-    const hotels = await softDeleteHotel(id) ; 
+    const hotels = await hotelRepository.softDelete(id) ; 
     return hotels ; 
 }
 
 export async function updateHotelServices(id : number , hotelData :updateHotelDTO){
-    const hotels = await updateHotel(id , hotelData) ; 
+    const hotels = await hotelRepository.update(id , hotelData) ; 
     return hotels ; 
 }
