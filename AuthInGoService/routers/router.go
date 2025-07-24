61c9847b1a4648a575df03a935237676729c2856
NRMT-MAIN <sahunirmit.main@gmail.com>
2025-07-24 20:48:44 +0530

    Implemented Reverse Proxy

--- AuthInGoService/routers/router.go
+++ AuthInGoService/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"AuthInGo/controllers"
 	"AuthInGo/middlewares"
+	"AuthInGo/utils"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -15,9 +16,11 @@ func SetupRouter(UserRouter Router) *chi.Mux{
 	chiRouter := chi.NewRouter()
 
 	chiRouter.Use(middlewares.RateLimiter)
+	chiRouter.HandleFunc("/fake-product-service/*" , utils.ReverseProxy("https://fakestoreapi.in" , "fake-product-service" ,"api"))
 	chiRouter.Get("/ping" , controllers.PingHandeler)
 
 	UserRouter.Register(chiRouter)
 
 	return chiRouter
 }
+//https://fakestoreapi.in/api/products 
